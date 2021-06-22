3892c6598570d2f7156fc1c66c29af6e969ad5c8
Sanju Baloria <sanjubaloria12@gmail.com>
2021-06-22 14:04:49 +0530

    Add Change

--- src/Yutlink/Lang.jsx
+++ src/Yutlink/Lang.jsx
@@ -12,140 +12,140 @@ function Lang(){
             </div>
             </div>
             <div id="cards_landscape_wrap-2">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                    <a href="">
-                        <div class="card-flyer">
-                            <div class="text-box">
-                                <div class="image-box">
-                                    <img src="https://i.pinimg.com/originals/6e/8d/73/6e8d73d6c0f7f21f38f5ceed494c357d.png" alt="C TUTORIAL" />
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <a href="">
+                                <div class="card-flyer">
+                                    <div class="text-box">
+                                        <div class="image-box">
+                                            <img src="https://i.pinimg.com/originals/6e/8d/73/6e8d73d6c0f7f21f38f5ceed494c357d.png" alt="C TUTORIAL" />
+                                        </div>
+                                        <div class="text-container">
+                                            <h6>C TUTORIAL</h6>
+                                            <p><i>This Card Contain the Best Youtube Tutorial For Learning the Fundamental of the C Programming Language</i></p>
+                                            <a href="https://www.youtube.com/watch?v=rLf3jnHxSmU&list=PLBlnK6fEyqRggZZgYpPMUxdY1CYkZtARR" target="_blank" class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="text-container">
-                                    <h6>C TUTORIAL</h6>
-                                    <p><i>This Card Contain the Best Youtube Tutorial For Learning the Fundamental of the C Programming Language</i></p>
-                                    <a href="https://www.youtube.com/watch?v=rLf3jnHxSmU&list=PLBlnK6fEyqRggZZgYpPMUxdY1CYkZtARR" target="_blank" class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
-                                </div>
-                            </div>
+                            </a>
                         </div>
-                    </a>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                    <a href="">
-                        <div class="card-flyer">
-                            <div class="text-box">
-                                <div class="image-box">
-                                    <img src="https://www.pngrepo.com/png/94391/512/c-logo.png" alt="C++ TUTORIAL" />
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <a href="">
+                                <div class="card-flyer">
+                                    <div class="text-box">
+                                        <div class="image-box">
+                                            <img src="https://www.pngrepo.com/png/94391/512/c-logo.png" alt="C++ TUTORIAL" />
+                                        </div>
+                                        <div class="text-container">                                    
+                                            <h6>C++ TUTORIAL</h6>
+                                            <p><i>This Card Contain the Best Youtube Tutorial For Learning the Fundamental of the C++ Programming Language</i></p>
+                                            <a href="https://www.youtube.com/watch?v=AKNGgAXTark&list=PLIY8eNdw5tW_o8gsLqNBu8gmScCAqKm2Q" target="_blank" class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="text-container">                                    
-                                    <h6>C++ TUTORIAL</h6>
-                                    <p><i>This Card Contain the Best Youtube Tutorial For Learning the Fundamental of the C++ Programming Language</i></p>
-                                    <a href="https://www.youtube.com/watch?v=AKNGgAXTark&list=PLIY8eNdw5tW_o8gsLqNBu8gmScCAqKm2Q" target="_blank" class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
-                                </div>
-                            </div>
+                            </a>
                         </div>
-                    </a>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                    <a href="">
-                        <div class="card-flyer">
-                            <div class="text-box">
-                                <div class="image-box">
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_1VCAMNKEwwuESo-spRyJtazDX2a806skAA&usqp=CAU" alt="PYTHON TUTORIAL" />
-                                </div>
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <a href="">
+                                <div class="card-flyer">
+                                    <div class="text-box">
+                                        <div class="image-box">
+                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_1VCAMNKEwwuESo-spRyJtazDX2a806skAA&usqp=CAU" alt="PYTHON TUTORIAL" />
+                                        </div>
 
-                                <div class="text-container">
-                                    <h6>PYTHON TUTORIAL</h6>
-                                    <p><i>This Card Contain the Best Youtube Tutorial For Learning the Fundamental of the Python Programming Language</i></p>
-                                    <a href="https://www.youtube.com/watch?v=_uQrJ0TkZlc" target="_blank" class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
+                                        <div class="text-container">
+                                            <h6>PYTHON TUTORIAL</h6>
+                                            <p><i>This Card Contain the Best Youtube Tutorial For Learning the Fundamental of the Python Programming Language</i></p>
+                                            <a href="https://www.youtube.com/watch?v=_uQrJ0TkZlc" target="_blank" class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+                            </a>
                         </div>
-                    </a>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                    <a href="">
-                        <div class="card-flyer">
-                            <div class="text-box">
-                                <div class="image-box">
-                                    <img src="https://besthqwallpapers.com/Uploads/17-2-2020/122068/thumb2-java-glitter-logo-programming-language-grid-metal-background-java-creative.jpg" alt="JAVA TUTORIAL" />
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <a href="">
+                                <div class="card-flyer">
+                                    <div class="text-box">
+                                        <div class="image-box">
+                                            <img src="https://besthqwallpapers.com/Uploads/17-2-2020/122068/thumb2-java-glitter-logo-programming-language-grid-metal-background-java-creative.jpg" alt="JAVA TUTORIAL" />
+                                        </div>
+                                        <div class="text-container">
+                                            <h6>JAVA TUTORIAL</h6>
+                                            <p><i>This Card Contain the Best Youtube Tutorial For Learning the Fundamental of the Java Programming Language</i></p>
+                                            <a href="https://www.youtube.com/watch?v=eIrMbAQSU34" target="_blank" class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="text-container">
-                                    <h6>JAVA TUTORIAL</h6>
-                                    <p><i>This Card Contain the Best Youtube Tutorial For Learning the Fundamental of the Java Programming Language</i></p>
-                                    <a href="https://www.youtube.com/watch?v=eIrMbAQSU34" target="_blank" class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
-                                </div>
-                            </div>
+                            </a>
                         </div>
-                    </a>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                    <a href="">
-                        <div class="card-flyer">
-                            <div class="text-box">
-                                <div class="image-box">
-                                    <img src="https://i.pinimg.com/originals/6e/8d/73/6e8d73d6c0f7f21f38f5ceed494c357d.png" alt="C STUDY" />
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <a href="">
+                                <div class="card-flyer">
+                                    <div class="text-box">
+                                        <div class="image-box">
+                                            <img src="https://i.pinimg.com/originals/6e/8d/73/6e8d73d6c0f7f21f38f5ceed494c357d.png" alt="C STUDY" />
+                                        </div>
+                                        <div class="text-container">
+                                            <h6>C STUDY MATERIAL</h6>
+                                            <p><i>This Card Contain the Best Study Material  For Learning the Fundamental of the C Programming Language</i></p>
+                                            <a href="https://www.tutorialspoint.com/cprogramming/index.htm" target="_blank" class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="text-container">
-                                    <h6>C STUDY MATERIAL</h6>
-                                    <p><i>This Card Contain the Best Study Material  For Learning the Fundamental of the C Programming Language</i></p>
-                                    <a href="https://www.tutorialspoint.com/cprogramming/index.htm" target="_blank" class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
-                                </div>
-                            </div>
+                            </a>
                         </div>
-                    </a>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                    <a href="">
-                        <div class="card-flyer">
-                            <div class="text-box">
-                                <div class="image-box">
-                                    <img src="https://www.pngrepo.com/png/94391/512/c-logo.png" alt="C++ STUDY" />
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <a href="">
+                                <div class="card-flyer">
+                                    <div class="text-box">
+                                        <div class="image-box">
+                                            <img src="https://www.pngrepo.com/png/94391/512/c-logo.png" alt="C++ STUDY" />
+                                        </div>
+                                        <div class="text-container">
+                                            <h6>C++ STUDY MATERIAL</h6>
+                                            <p><i>This Card Contain the BestStudy Material  For Learning the Fundamental of the C++ Programming Language</i></p>
+                                            <a href="https://www.w3schools.com/CPP/default.asp" target="_blank"  class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="text-container">
-                                    <h6>C++ STUDY MATERIAL</h6>
-                                    <p><i>This Card Contain the BestStudy Material  For Learning the Fundamental of the C++ Programming Language</i></p>
-                                    <a href="https://www.w3schools.com/CPP/default.asp" target="_blank"  class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
-                                </div>
-                            </div>
+                            </a>
                         </div>
-                    </a>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                    <a href="">
-                        <div class="card-flyer">
-                            <div class="text-box">
-                                <div class="image-box">
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_1VCAMNKEwwuESo-spRyJtazDX2a806skAA&usqp=CAU" alt="PYTHON STUDY"/>
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <a href="">
+                                <div class="card-flyer">
+                                    <div class="text-box">
+                                        <div class="image-box">
+                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_1VCAMNKEwwuESo-spRyJtazDX2a806skAA&usqp=CAU" alt="PYTHON STUDY"/>
+                                        </div>
+                                        <div class="text-container">
+                                            <h6>PYTHON STUDY MATERIAL</h6>
+                                            <p><i>This Card Contain the Best Study Material For Learning the Fundamental of the Python Programming Language</i></p>
+                                            <a href="https://www.w3schools.com/python/default.asp" target="_blank" class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="text-container">
-                                    <h6>PYTHON STUDY MATERIAL</h6>
-                                    <p><i>This Card Contain the Best Study Material For Learning the Fundamental of the Python Programming Language</i></p>
-                                    <a href="https://www.w3schools.com/python/default.asp" target="_blank" class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
-                                </div>
-                            </div>
+                            </a>
                         </div>
-                    </a>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                    <a href="">
-                        <div class="card-flyer">
-                            <div class="text-box">
-                                <div class="image-box">
-                                    <img src="https://besthqwallpapers.com/Uploads/17-2-2020/122068/thumb2-java-glitter-logo-programming-language-grid-metal-background-java-creative.jpg" alt="JAVA STUDY" />
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <a href="">
+                                <div class="card-flyer">
+                                    <div class="text-box">
+                                        <div class="image-box">
+                                            <img src="https://besthqwallpapers.com/Uploads/17-2-2020/122068/thumb2-java-glitter-logo-programming-language-grid-metal-background-java-creative.jpg" alt="JAVA STUDY" />
+                                        </div>
+                                        <div class="text-container">
+                                            <h6>JAVA STUDY MATERIAL</h6>
+                                            <p><i>This Card Contain the BestStudy Material  For Learning the Fundamental of the Java Programming Language</i></p>
+                                            <a href="https://www.w3schools.com/java/default.asp" target="_blank" class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="text-container">
-                                    <h6>JAVA STUDY MATERIAL</h6>
-                                    <p><i>This Card Contain the BestStudy Material  For Learning the Fundamental of the Java Programming Language</i></p>
-                                    <a href="https://www.w3schools.com/java/default.asp" target="_blank" class="btn btn-primary" style={{marginTop: "10px",borderRadius:"20px"}}>Click to Start</a>
-                                </div>
-                            </div>
+                            </a>
                         </div>
-                    </a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
         </>
     );
 
