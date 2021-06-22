db1f6893ab4b9a723dc44769eae16fe2ad290470
Sanju Baloria <sanjubaloria12@gmail.com>
2021-06-22 14:01:14 +0530

    Add Change

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import reactDom from 'react-dom';
 import {Route,Switch,Redirect} from "react-router-dom";
 import Login from "./Signup/Login";
-import Create from "./Signup/Create"
-
+import Create from "./Signup/Create";
+import Lang from "./Yutlink/Lang";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 function App(){
     return(
         <>
             <Switch>
             <Route exact path="/" component={Create}/>
-            <Route path="/login"  component={Login}/>
+            <Route exact path="/login"  component={Login}/>
+            <Route path="/lang"  component={Lang}/>
             <Redirect to="/"/>
             </Switch>
         </>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Login from "./Signup/Login";
-import Create from "./Signup/Create"
 import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
